38c6256b34434b7420e980a56a36fa2191f5155c
CJ Managuelod <cjmanaguelod1895@outlook.com>
2020-12-28 13:50:35 +0800

    Update Brgy table.

--- InventoryDB/dbo/Tables/Brgy.sql
+++ InventoryDB/dbo/Tables/Brgy.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[Brgy] (
-    [BaranggayId]          INT           IDENTITY (1, 1) NOT NULL,
-    [BaranggayCode]        VARCHAR (255) NULL,
-    [BaranggayDescription] TEXT          NULL,
+    [BarangayId]           INT           IDENTITY (1, 1) NOT NULL,
+    [BarangayCode]         VARCHAR (255) NULL,
+    [BarangayDescription]  TEXT          NULL,
     [RegCode]              VARCHAR (255) NULL,
     [ProvinceCode]         VARCHAR (255) NULL,
     [CityMunicipalityCode] VARCHAR (255) NULL,
-    CONSTRAINT [PK_Brgy] PRIMARY KEY CLUSTERED ([BaranggayId] ASC)
+    CONSTRAINT [PK_Brgy] PRIMARY KEY CLUSTERED ([BarangayId] ASC)
 );
+
+
 
